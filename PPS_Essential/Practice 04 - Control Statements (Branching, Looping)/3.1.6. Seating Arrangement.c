c5e98d79842fcf0466e7009b9b848d488bc65eee
jermgaming <atinder999@gmail.com>
2025-09-19 13:20:46 +0530

    3.1.6-essential

---
+++ PPS_Essential/Practice 04 - Control Statements (Branching, Looping)/3.1.6. Seating Arrangement.c
@@ -0,0 +1,16 @@
+//Write the code here.
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			if(i%2==0){
+				printf("Pass ");
+			}else {
+				printf("Fail ");
+			}
+		}printf("\n");
+	}
+	return 0;
+}
