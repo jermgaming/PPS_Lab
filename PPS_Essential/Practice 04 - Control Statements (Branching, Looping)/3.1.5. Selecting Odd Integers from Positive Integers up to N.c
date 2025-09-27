9d8fe17dc65006d6c3784a407d4f3ae767008713
jermgaming <atinder999@gmail.com>
2025-09-27 20:54:52 +0530

    3.1.5-essential

--- PPS_Essential/Practice 04 - Control Statements (Branching, Looping)/3.1.5. Selecting Odd Integers from Positive Integers up to N.c
+++ PPS_Essential/Practice 04 - Control Statements (Branching, Looping)/3.1.5. Selecting Odd Integers from Positive Integers up to N.c
@@ -2,6 +2,12 @@
 #include<stdio.h>
 int main(){
 	int n;
-	n%2==0?printf("%0.5f",((n/2)/n):printf("%0.5f",((n/2)/n);
+	scanf("%d",&n);
+	if(n%2==0){
+		printf("%0.5f\n",(n/2.0)/n*1.0);
+	}else {
+		int temp=n/2;
+		printf("%0.5f\n",(temp+1.0)/n);
+	}
 	return 0;
 }
