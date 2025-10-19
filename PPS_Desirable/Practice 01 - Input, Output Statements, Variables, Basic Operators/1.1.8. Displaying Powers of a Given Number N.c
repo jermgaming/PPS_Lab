d45c28a76053ae127c2a15de1365a5d2da31ebc8
jermgaming <atinder999@gmail.com>
2025-10-19 11:18:41 +0530

    1.1.8-desirable

---
+++ PPS_Desirable/Practice 01 - Input, Output Statements, Variables, Basic Operators/1.1.8. Displaying Powers of a Given Number N.c
@@ -0,0 +1,9 @@
+// Write your complete code here
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a;
+	scanf("%d",&a);
+	printf("%d %d %d",(int)pow(a,1),(int)pow(a,2),(int)pow(a,3));
+	return 0;
+}
