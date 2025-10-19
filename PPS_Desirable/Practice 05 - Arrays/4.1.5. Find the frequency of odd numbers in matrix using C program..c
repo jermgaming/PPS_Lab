da6da8a0735c2142daf731aab8025c73ffe8046e
jermgaming <atinder999@gmail.com>
2025-10-19 09:29:08 +0530

    4.1.5-desirable

---
+++ PPS_Desirable/Practice 05 - Arrays/4.1.5. Find the frequency of odd numbers in matrix using C program..c
@@ -0,0 +1,20 @@
+//Write your code here
+//Write your code here
+#include<stdio.h>
+int main(){
+	int n,m;
+	scanf("%d%d",&n,&m);
+	int a[n][m];
+	int ans=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			scanf("%d",&a[n][m]);
+			if(a[n][m]%2==1){
+				ans++;
+			}
+		}
+	}
+	printf("%d",ans);
+	
+	return 0;
+}
