fc90d0d88dbe96464a8a53c258ecc41fb4dc0468
jermgaming <atinder999@gmail.com>
2025-11-03 09:53:39 +0530

    3.1.5_lab

---
+++ PPS_Lab/Example-3/3.1.5. Sum of series using functions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int fact(int n){
+	if(n==0 | n==1) return 1;
+	return n*fact(n-1);
+}
+// Function to calculate the sum of the series
+double calculateSeries(double x, int n) {
+    int sign=1,power=1;
+	double ans=0;
+	for(int i=1;i<=n;i++){
+		double temp=pow(x,power)/fact(power);
+		sign=(i%2==0)?-1:1;
+		temp=sign*temp;
+		ans+=temp;
+		power=power+2;
+	}
+	return ans;
+}
+
+int main() {
+    double x;
+    int n;
+    scanf("%lf", &x);
+    scanf("%d", &n);
+    double result = calculateSeries(x, n);
+    printf("%.4lf\n", result);
+    return 0;
+}
